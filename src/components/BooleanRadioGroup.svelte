259d7dbe3100cde86ad47582d66ba71790cd1140
Markichu <31204091+Markichu@users.noreply.github.com>
2023-05-23 10:01:26 +1000

    Style the input to dynamically change size

--- src/components/BooleanRadioGroup.svelte
+++ src/components/BooleanRadioGroup.svelte
@@ -4,28 +4,31 @@
 	export let value: number;
 
 	let css = {
-		input_group: 'input-group btn-group h-12 w-fit my-1',
-		input_text: 'w-64',
-		boolean_button: 'btn w-32'
+		input_group: 'flex flex-wrap align-left flex-row justify-between rounded bg-base-300 h-fit min-h-16 w-5/6 md:w-2/3 lg:w-1/3 m-1 p-1',
+		input_text: 'text-center my-auto basis-full md:basis-1/2',
+		boolean_button: 'btn w-1/2',
+		btn_group: "btn-group basis-full md:basis-1/2 my-auto",
 	};
 </script>
 
 <label class={css.input_group}>
 	<span class={css.input_text}>{label}</span>
-	<input
+	<div class={css.btn_group}>
+		<input
 		class={css.boolean_button}
 		type="radio"
 		{name}
 		data-title="Yes"
 		checked={value === 1}
 		on:change={() => (value = 1)}
-	/>
-	<input
-		class={css.boolean_button}
-		type="radio"
-		{name}
-		data-title="No"
-		checked={value === 0}
-		on:change={() => (value = 0)}
-	/>
+		/>
+		<input
+			class={css.boolean_button}
+			type="radio"
+			{name}
+			data-title="No"
+			checked={value === 0}
+			on:change={() => (value = 0)}
+		/>
+	</div>
 </label>
