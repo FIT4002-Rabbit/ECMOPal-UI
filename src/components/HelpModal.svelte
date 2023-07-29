c6521dfc7568f790bb6aaf2efdc741c850df5d44
Markichu <31204091+Markichu@users.noreply.github.com>
2023-07-29 16:56:22 +1000

    Run Prettier

--- src/components/HelpModal.svelte
+++ src/components/HelpModal.svelte
@@ -8,18 +8,26 @@
 		slider_range: 'range w-4/5 mx-2 my-auto'
 	};
 
-    let is_open: boolean = false;
+	let is_open: boolean = false;
 </script>
 
-<button class="bg-base-100 rounded-sm w-6" on:click={() => is_open = true}>?</button>
+<button class="bg-base-100 rounded-sm w-6" on:click={() => (is_open = true)}>?</button>
 {#if is_open === true}
-    <!-- Black background that blocks interaction -->
-    <div class="fixed z-50 left-0 top-0 w-full h-full bg-black/[.25] flex align-middle" on:mousedown={() => is_open = false}>
-
-        <!-- Content box for modal -->
-        <div on:mousedown|stopPropagation={()=>{}} class="relative w-2/3 lg:w-1/2 m-auto p-10 bg-base-100 rounded-md text-left">
-            <button class="absolute top-0 right-0 w-8 h-8 m-2 rounded-sm bg-base-300" on:click={() => is_open = false}>X</button>
-            <slot />
-        </div>
-    </div>
+	<!-- Black background that blocks interaction -->
+	<div
+		class="fixed z-50 left-0 top-0 w-full h-full bg-black/[.25] flex align-middle"
+		on:mousedown={() => (is_open = false)}
+	>
+		<!-- Content box for modal -->
+		<div
+			on:mousedown|stopPropagation={() => {}}
+			class="relative w-2/3 lg:w-1/2 m-auto p-10 bg-base-100 rounded-md text-left"
+		>
+			<button
+				class="absolute top-0 right-0 w-8 h-8 m-2 rounded-sm bg-base-300"
+				on:click={() => (is_open = false)}>X</button
+			>
+			<slot />
+		</div>
+	</div>
 {/if}
