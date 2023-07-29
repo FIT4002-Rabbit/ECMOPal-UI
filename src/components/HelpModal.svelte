20d0ea70b58b7d01ebad9bb14731eb25b82089ea
Markichu <31204091+Markichu@users.noreply.github.com>
2023-07-30 02:32:53 +1000

    Add transitions to help modal

--- src/components/HelpModal.svelte
+++ src/components/HelpModal.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { fade, scale } from 'svelte/transition';
+
 	let is_open = false;
 </script>
 
@@ -6,13 +8,15 @@
 {#if is_open === true}
 	<!-- Black background that blocks interaction -->
 	<div
-		class="fixed z-50 left-0 top-0 w-full h-full bg-black/[.25] flex align-middle"
+		transition:fade={{ duration: 150 }}
+		class="fixed z-50 left-0 top-0 w-full h-full bg-black/[.5] flex align-middle"
 		on:mousedown={() => (is_open = false)}
 	>
 		<!-- Content box for modal -->
 		<div
+			transition:scale={{ duration: 150 }}
 			on:mousedown|stopPropagation={() => undefined}
-			class="relative w-2/3 lg:w-1/2 m-auto p-10 bg-base-100 rounded-md text-left"
+			class="relative w-2/3 lg:w-1/2 m-auto p-10 bg-base-100 rounded-md text-left whitespace-pre-line"
 		>
 			<button
 				class="absolute top-0 right-0 w-8 h-8 m-2 rounded-sm bg-base-300"
