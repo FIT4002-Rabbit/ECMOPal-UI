a048cbfc8a1b9265e150594fbc15b620c1cc499c
Christian Yaacoub <cyaa0001@student.monash.edu>
2023-08-08 19:51:13 +1000

    run prettier

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,80 +1,78 @@
 import { test, expect } from '@playwright/test';
-import type { PatientData } from '../src/types/PatientData';
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+	await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/ECMO PAL Lite/);
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/ECMO PAL Lite/);
 });
 
 test('Check that clicking "no" button deselects "yes" button', async ({ page }) => {
-  await page.goto('/');
+	await page.goto('/');
 
-  const yesButton = page.getByRole("radio").nth(0);
-  const noButton = page.getByRole("radio").nth(1);
+	const yesButton = page.getByRole('radio').nth(0);
+	const noButton = page.getByRole('radio').nth(1);
 
-  await expect(yesButton).toBeChecked();  // check "yes" is checked by default
-  await expect(noButton).not.toBeChecked();  // check "no" is unchecked by default
+	await expect(yesButton).toBeChecked(); // check "yes" is checked by default
+	await expect(noButton).not.toBeChecked(); // check "no" is unchecked by default
 
-  await noButton.check();  // click "no" button
-  
-  // Check that "no" button is checked and "yes" button is unchecked
-  await expect(yesButton).not.toBeChecked();
-  await expect(noButton).toBeChecked();
+	await noButton.check(); // click "no" button
 
-  await yesButton.check();  // click "yes" button
+	// Check that "no" button is checked and "yes" button is unchecked
+	await expect(yesButton).not.toBeChecked();
+	await expect(noButton).toBeChecked();
 
-  // Check that "yes" button is checked and "no" button is unchecked
-  await expect(noButton).not.toBeChecked();
-  await expect(yesButton).toBeChecked();
+	await yesButton.check(); // click "yes" button
+
+	// Check that "yes" button is checked and "no" button is unchecked
+	await expect(noButton).not.toBeChecked();
+	await expect(yesButton).toBeChecked();
 });
 
 test('Check "yes" button is checked by default', async ({ page }) => {
-  await page.goto('/');
+	await page.goto('/');
 
-  const yesButton = page.getByRole("radio").nth(0);
-  const noButton = page.getByRole("radio").nth(1);
+	const yesButton = page.getByRole('radio').nth(0);
+	const noButton = page.getByRole('radio').nth(1);
 
-  await expect(yesButton).toBeChecked();  // check "yes" is checked by default
-  await expect(noButton).not.toBeChecked();  // check "no" is unchecked by default
+	await expect(yesButton).toBeChecked(); // check "yes" is checked by default
+	await expect(noButton).not.toBeChecked(); // check "no" is unchecked by default
 });
 
 test('Check slider min and max values', async ({ page }) => {
-  await page.goto('/');
+	await page.goto('/');
 
-  const slider = page.getByTestId("Intubation Time (Hours) slider");
-  const input_box = page.getByTestId("Intubation Time (Hours) number");
+	const slider = page.getByTestId('Intubation Time (Hours) slider');
 
-  const box = await slider.boundingBox();
+	const box = await slider.boundingBox();
 
-  await slider.click({
-    position: {
-      x: box?.width ? box.width-10 : 0,
-      y: box?.height ? box.height/2 : 0
-    }
-  });
-  
-  await expect(slider).toHaveValue("672");
+	await slider.click({
+		position: {
+			x: box?.width ? box.width - 10 : 0,
+			y: box?.height ? box.height / 2 : 0
+		}
+	});
 
-  await slider.click({
-    position: {
-      x: 1,
-      y: box?.height ? box.height/2 : 0
-    }
-  });
+	await expect(slider).toHaveValue('672');
 
-  await expect(slider).toHaveValue("1");
+	await slider.click({
+		position: {
+			x: 1,
+			y: box?.height ? box.height / 2 : 0
+		}
+	});
+
+	await expect(slider).toHaveValue('1');
 });
 
 test('Text fields limited to numeric inputs', async ({ page }) => {
-  await page.goto('/');
+	await page.goto('/');
 
-  const input_box = page.getByTestId("Intubation Time (Hours) number");
+	const input_box = page.getByTestId('Intubation Time (Hours) number');
 
-  await input_box.fill("52");
+	await input_box.fill('52');
 
-  await expect(input_box).toHaveValue("52");
+	await expect(input_box).toHaveValue('52');
 
-  await expect(input_box).toHaveAttribute("type", "number");
+	await expect(input_box).toHaveAttribute('type', 'number');
 });
