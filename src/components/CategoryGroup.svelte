017c7500b9af894c00e6e0aa7e4e3af9d0fbf89e
JMSS-Unknown <31131631+JMSS-Unknown@users.noreply.github.com>
2023-09-02 14:31:28 +1000

    Make features flow as columns instead of as rows

--- src/components/CategoryGroup.svelte
+++ src/components/CategoryGroup.svelte
@@ -8,8 +8,8 @@
 </script>
 
 <div class="w-full">
-	<h2 class="text-2xl text-secondary">{category.name}</h2>
-	<div class="grid grid-cols-1 sm:grid-cols-2 gap-2">
+	<h2 class="text-2xl text-secondary ms-1 sm:ms-0 font-bold">{category.name}</h2>
+	<div class="columns-1 sm:columns-2 gap-2 space-y-2">
 		{#each category.features as feature}
 			{#if feature.type === 'boolean'}
 				<BooleanRadioGroup {feature} bind:patientData />
